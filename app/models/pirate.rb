b4eb0cc1a04388c2cc0f2b66a830870877e53ab1
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-04-22 22:28:11 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -4,6 +4,10 @@ class Pirate
   @@all = []
 
   def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    @@all << self
   end
 
   def self.all
