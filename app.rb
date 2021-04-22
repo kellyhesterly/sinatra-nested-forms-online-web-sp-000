6874558179c8f74d5e6fd48c902d23a0be7470b9
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-04-22 22:02:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,6 +4,19 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      "Welcome to the Nested Forms Lab!"
+    end
 
-  end
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+
+      @ships = Ship.all
+      erb :show
+    end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,14 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all.clear
+  end
+
 end
